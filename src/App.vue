791a1a9e2ab2a7f824b9a4ea95e5200db2312f21
almo16 <almaobetzabeth@gmail.com>
2022-08-09 16:35:44 -0500

    First commit

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,118 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="Vue logo" src="./assets/logo.png" width="80px" height="80px" />
+    <HelloWorld msg="Calculadora Vue.js" />
+
+    <article>
+      <!-- numero 1 -->
+      <label for="">Número 1: </label>
+      <input type="number" v-model="numero1" name="" id="" required />
+      <br />
+      <br />
+
+      <!-- numero 1 -->
+      <label for="">Número 2: </label>
+      <input type="number" v-model="numero2" name="" id="" required />
+      <br />
+      <br />
+
+      <!-- Operacion-->
+      <label for="">Operación: </label>
+      <select  v-model="operacionSelec" id="operacion">
+        <option
+          v-for="operacion in operaciones"
+          :value="operacion"
+          :key="operacion.id"
+        >
+          {{ operacion.nombre }}
+        </option>
+      </select>
+      <br />
+      <br />
+
+       
+      <!-- Respuesta  -->
+      <label for="">Resultado: </label>
+      <input type="number" v-model="resultado" name="" id="" />
+      <br />
+      <br />
+
+      <!-- Boton Calcular -->
+      <button  @click="realizarOperacion()">Calcular</button>
+      <br />
+      <br />
+    </article>
+
+    <TablaOperaciones operaciones="operacionesRealizadas"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from "./components/HelloWorld.vue";
+import TablaOperaciones from './components/TablaOperaciones.vue';
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
+    HelloWorld,
+    TablaOperaciones,
+  },
+   data: () =>({
+    numero1: '',
+    numero2: '',
+    resultado: '',
+    operaciones: [
+      { id: 1, nombre: 'Suma'},
+      { id: 2, nombre: 'Resta'},
+      { id: 3, nombre: 'Multiplicación'},
+      { id: 4, nombre: 'División'},
+    ],
+    operacionSelec: {},
+    operacionesRealizadas: []
+
+  }),
+  methods: {
+
+    realizarOperacion(){
+             if(this.operacionSelec.id === 1){
+          this.resultado = Number(this.numero1) + Number(this.numero2);
+       }
+       else {
+        if(this.operacionSelec.id === 2){
+          this.resultado =  Number(this.numero1) -  Number(this.numero2);
+          }else{
+            if(this.operacionSelec.id === 3){
+          this.resultado =  Number(this.numero1) *  Number(this.numero2);
+            }
+            else{
+          this.resultado =  Number(this.numero1) /  Number(this.numero2);
+              }
+          }
+       }
+    
+
+       let operacion =''
+      switch(this.operacionSelec.id)
+      {
+        case 1:
+          operacion = "Suma"
+          break;
+          case 2:
+             operacion = "Resta"
+          break;
+          case 3:
+             operacion = "Multiplicación"
+          break;
+          default:
+             operacion = "División"
+      }
+
+      this.operacionesRealizadas.push(operacion,this.numero1,this.numero2,this.resultado);
+    },
+
+    
   }
-}
+};
 </script>
 
 <style>
---
+++ src/components/TablaOperaciones.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="">
+   <article>
+     <table class="default" align="center">
+      <tr>
+        <th>Operación</th>
+        <th>Número 1</th>
+        <th>Número 2</th>
+        <th>Respuesta</th>
+      </tr>
+
+      
+        <tr>
+                          
+ 
+                            <td>{{this.operaciones.i}}</td>
+                            <td>{{item.cantidadc}}</td>
+                            <td>{{item.precioc}}</td>
+                            <td>{{item.cantidadc*item.precioc}}</td>
+  </tr>
+      
+    </table>
+   </article>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "TablaOperaciones",
+  props: {
+    operaciones: [{
+      operacion : '',
+      num1 : '',
+      num2 : '',
+      
+
+    }
+    ]
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+table {
+  background-color: rgb(30, 233, 206);
+  color: black;
+}
+
+
+</style>
